ea85f38a9641e34ef973155af104cf0bd1d0dc81
errt <helm@cs.tu-darmstadt.de>
2026-06-10 20:46:43 +0200

    Vorteil/Nachteil/SF-Voraussetzungen für RKP

--- src/chargen/pros_cons_skills/ProConSkillSelectors.java
+++ src/chargen/pros_cons_skills/ProConSkillSelectors.java
@@ -18,11 +18,16 @@ package chargen.pros_cons_skills;
 import java.util.ArrayList;
 import java.util.List;
 
+import chargen.race_culture_profession.BGBVeteranSelector.BGBVeteran;
 import chargen.ui.TabController;
 import chargen.util.ChargenUtil;
 import dsa41basis.util.HeroUtil;
+import dsa41basis.util.RequirementsUtil;
+import dsatool.resources.ResourceManager;
 import dsatool.resources.Settings;
+import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
@@ -31,7 +36,9 @@ import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.control.Tooltip;
 import javafx.scene.layout.VBox;
+import jsonant.event.JSONListener;
 import jsonant.value.JSONArray;
 import jsonant.value.JSONObject;
 
@@ -47,10 +54,22 @@ public class ProConSkillSelectors extends TabController {
 
 	private final VBox leftBox;
 
+	private Label raceLabel;
+	private Label cultureLabel;
+	private Label professionLabel;
+	private Label bgbVeteranLabel;
+	private Label bgbVeteranProfessionLabel;
+
+	private final CheckBox showAll = new CheckBox("Alle anzeigen");
+
 	private final IntegerProperty conGP = new SimpleIntegerProperty();
 	private final IntegerProperty seGP = new SimpleIntegerProperty();
 
-	private final CheckBox showAll = new CheckBox("Alle anzeigen");
+	private final BooleanProperty hasUnfulfilledRequirements = new SimpleBooleanProperty(false);
+
+	protected final JSONListener listener = _ -> {
+		updateRequirements();
+	};
 
 	public ProConSkillSelectors(final JSONObject generationState, final TabPane tabPane, final VBox leftBox, final IntegerProperty gp) {
 		super(generationState, gp);
@@ -72,13 +91,78 @@ public class ProConSkillSelectors extends TabController {
 
 		final ObservableList<Node> items = leftBox.getChildren();
 
+		final Node spacer = items.remove(0);
+
 		items.add(0, showAll);
+		VBox.setMargin(showAll, new Insets(0, 0, 20, 0));
 
 		final JSONObject hero = generationState.getObj("Held");
 
 		cleanupCheaperSkills(hero);
 
-		int pos = 2;
+		final JSONObject biography = hero.getObj("Biografie");
+
+		items.add(1, new Label("Rasse: "));
+		raceLabel = new Label();
+		raceLabel.setWrapText(true);
+		VBox.setMargin(raceLabel, new Insets(0, 0, 0, 10));
+		final StringBuilder raceString = new StringBuilder(biography.getStringOrDefault("Rasse", ""));
+		if (biography.containsKey("Rasse:Modifikation")) {
+			final JSONArray raceModifiers = biography.getArr("Rasse:Modifikation");
+			raceString.append(" (");
+			raceString.append(String.join(", ", raceModifiers.getStrings()));
+			raceString.append(")");
+		}
+		raceLabel.setText(raceString.toString());
+		items.add(2, raceLabel);
+
+		final JSONObject professions = ResourceManager.getResource("data/Professionen");
+
+		items.add(3, new Label("Kultur: "));
+		cultureLabel = new Label();
+		cultureLabel.setWrapText(true);
+		VBox.setMargin(cultureLabel, new Insets(0, 0, 0, 10));
+		final StringBuilder cultureString = new StringBuilder(biography.getStringOrDefault("Kultur", ""));
+		if (biography.containsKey("Kultur:Modifikation")) {
+			final JSONArray cultureModifiers = biography.getArr("Kultur:Modifikation");
+			cultureString.append(" (");
+			cultureString.append(String.join(", ", cultureModifiers.getStrings()));
+			cultureString.append(")");
+		}
+		cultureLabel.setText(cultureString.toString());
+		items.add(4, cultureLabel);
+
+		items.add(5, new Label("Profession: "));
+		professionLabel = new Label();
+		professionLabel.setWrapText(true);
+		VBox.setMargin(professionLabel, new Insets(0, 0, 0, 10));
+		professionLabel.setText(HeroUtil.getProfessionString(hero, biography, professions, false));
+		items.add(6, professionLabel);
+
+		updateRequirements();
+		hero.getObj("Vorteile").addListener(listener);
+		hero.getObj("Nachteile").addListener(listener);
+		hero.getObj("Sonderfertigkeiten").addListener(listener);
+
+		int pos = 7;
+
+		if (generationState.containsKey("Breitgefächerte Bildung") || generationState.containsKey("Veteran")) {
+			bgbVeteranLabel = new Label();
+			items.add(pos, bgbVeteranLabel);
+			bgbVeteranProfessionLabel = new Label();
+			bgbVeteranProfessionLabel.setWrapText(true);
+			VBox.setMargin(bgbVeteranProfessionLabel, new Insets(0, 0, 0, 10));
+			final BGBVeteran type = generationState.containsKey("Veteran") ? BGBVeteran.VETERAN : BGBVeteran.BGB;
+			final String variantString = HeroUtil.getVeteranBGBString(hero, biography, professions).split(" ", 2)[1];
+			bgbVeteranLabel.setText(type == BGBVeteran.BGB ? "Breitgefächerte Bildung:" : variantString.length() != 0 ? "Veteran:" : "Veteran");
+			bgbVeteranProfessionLabel.setText(variantString);
+			items.add(pos + 1, bgbVeteranProfessionLabel);
+			pos += 2;
+		}
+
+		items.add(pos, spacer);
+		++pos;
+
 		final JSONObject pros = hero.getObj("Vorteile");
 		final int proGPPool = pros.getIntOrDefault("temporary:Pool", 0);
 		if (proGPPool > 0) {
@@ -121,6 +205,16 @@ public class ProConSkillSelectors extends TabController {
 		VBox.setMargin(cheaperSkillAPLabel, new Insets(0, 0, 0, 10));
 		pos += 2;
 
+		final int maxSEGP = Settings.getSettingIntOrDefault(30, "Heldenerschaffung", "GP aus Schlechten Eigenschaften");
+		seGP.set(0);
+
+		items.add(pos, new Label("GP aus Schlechten Eigenschaften: "));
+		final Label seGPLabel = new Label();
+		seGPLabel.textProperty().bind(seGP.asString().concat("/" + maxSEGP));
+		VBox.setMargin(seGPLabel, new Insets(0, 0, 0, 10));
+		items.add(pos + 1, seGPLabel);
+		pos += 2;
+
 		final int maxConGP = Settings.getSettingIntOrDefault(50, "Heldenerschaffung", "GP aus Nachteilen");
 		conGP.set(0);
 
@@ -130,23 +224,15 @@ public class ProConSkillSelectors extends TabController {
 		VBox.setMargin(conGPLabel, new Insets(0, 0, 0, 10));
 		items.add(pos + 1, conGPLabel);
 
-		final int maxSEGP = Settings.getSettingIntOrDefault(30, "Heldenerschaffung", "GP aus Schlechten Eigenschaften");
-		seGP.set(0);
-
-		items.add(pos, new Label("GP aus Schlechten Eigenschaften: "));
-		final Label seGPLabel = new Label();
-		seGPLabel.textProperty().bind(seGP.asString().concat("/" + maxSEGP));
-		VBox.setMargin(seGPLabel, new Insets(0, 0, 0, 10));
-		items.add(pos + 1, seGPLabel);
-
 		proSelector.activate(forward);
 		conSelector.activate(forward);
 		skillSelector.activate(forward);
 
 		proTab.getTabPane().getSelectionModel().select(proTab);
 
-		canContinue.bind(gp.isEqualTo(0).and(conGP.lessThanOrEqualTo(maxConGP)).and(seGP.lessThanOrEqualTo(maxSEGP)).and(proSelector.getPool().isEqualTo(0))
-				.and(conSelector.getPool().isEqualTo(0)).and(skillSelector.getPool().isEqualTo(0)).and(skillSelector.getCheaperPool().greaterThanOrEqualTo(0)));
+		canContinue.bind(hasUnfulfilledRequirements.not().and(gp.isEqualTo(0)).and(conGP.lessThanOrEqualTo(maxConGP)).and(seGP.lessThanOrEqualTo(maxSEGP))
+				.and(proSelector.getPool().isEqualTo(0)).and(conSelector.getPool().isEqualTo(0)).and(skillSelector.getPool().isEqualTo(0))
+				.and(skillSelector.getCheaperPool().greaterThanOrEqualTo(0)));
 	}
 
 	private void cleanupCheaperSkills(final JSONObject hero) {
@@ -195,8 +281,64 @@ public class ProConSkillSelectors extends TabController {
 		conSelector.deactivate(forward);
 		skillSelector.deactivate(forward);
 
+		final JSONObject hero = generationState.getObj("Held");
+		hero.getObj("Vorteile").addListener(listener);
+		hero.getObj("Nachteile").addListener(listener);
+		hero.getObj("Sonderfertigkeiten").addListener(listener);
+
 		final ObservableList<Node> items = leftBox.getChildren();
 		items.remove(showAll);
 		items.remove(1, items.size() - 2);
+	}
+
+	public void updateRequirements() {
+		hasUnfulfilledRequirements.set(false);
+
+		final JSONObject hero = generationState.getObj("Held");
+
+		final JSONObject raceRequirements = generationState.getObj("Rasse").getObj("Voraussetzungen").getObj("Vorteile/Nachteile/Sonderfertigkeiten");
+		raceLabel.getStyleClass().remove("invalid");
+		if (RequirementsUtil.isProConSkillRequirementFulfilled(hero, raceRequirements, null, null)) {
+			raceLabel.setTooltip(null);
+		} else {
+			hasUnfulfilledRequirements.set(true);
+			raceLabel.setTooltip(new Tooltip(RequirementsUtil.unfulfilledProConSkillRequirements(hero, raceRequirements, null, null)));
+			raceLabel.getStyleClass().add("invalid");
+		}
+
+		final JSONObject cultureRequirements = generationState.getObj("Kultur").getObj("Voraussetzungen").getObj("Vorteile/Nachteile/Sonderfertigkeiten");
+		cultureLabel.getStyleClass().remove("invalid");
+		if (RequirementsUtil.isProConSkillRequirementFulfilled(hero, cultureRequirements, null, null)) {
+			cultureLabel.setTooltip(null);
+		} else {
+			hasUnfulfilledRequirements.set(true);
+			cultureLabel.setTooltip(new Tooltip(RequirementsUtil.unfulfilledProConSkillRequirements(hero, cultureRequirements, null, null)));
+			cultureLabel.getStyleClass().add("invalid");
+		}
+
+		final JSONObject professionRequirements = generationState.getObj("Profession").getObj("Voraussetzungen")
+				.getObj("Vorteile/Nachteile/Sonderfertigkeiten");
+		professionLabel.getStyleClass().remove("invalid");
+		if (RequirementsUtil.isProConSkillRequirementFulfilled(hero, professionRequirements, null, null)) {
+			professionLabel.setTooltip(null);
+		} else {
+			hasUnfulfilledRequirements.set(true);
+			professionLabel.setTooltip(new Tooltip(RequirementsUtil.unfulfilledProConSkillRequirements(hero, professionRequirements, null, null)));
+			professionLabel.getStyleClass().add("invalid");
+		}
+
+		if (generationState.containsKey("Breitgefächerte Bildung") || generationState.containsKey("Veteran")) {
+			final String type = generationState.containsKey("Veteran") ? "Veteran" : "Breitgefächerte Bildung";
+			final JSONObject bgbVeteranRequirements = generationState.getObj(type).getObj("Voraussetzungen").getObj("Vorteile/Nachteile/Sonderfertigkeiten");
+			bgbVeteranProfessionLabel.getStyleClass().remove("invalid");
+			if (RequirementsUtil.isProConSkillRequirementFulfilled(hero, bgbVeteranRequirements, null, null)) {
+				bgbVeteranProfessionLabel.setTooltip(null);
+			} else {
+				hasUnfulfilledRequirements.set(true);
+				bgbVeteranProfessionLabel
+						.setTooltip(new Tooltip(RequirementsUtil.unfulfilledProConSkillRequirements(hero, bgbVeteranRequirements, null, null)));
+				bgbVeteranProfessionLabel.getStyleClass().add("invalid");
+			}
+		}
 	}
 }
